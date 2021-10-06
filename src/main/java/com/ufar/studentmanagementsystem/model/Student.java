026afe5a241099ded0c1ae191c5e21a09569cf88
Albina Hakobyan <albina.hakobyan7@gmail.com>
2021-10-07 00:07:21 +0400

    Fields

--- src/main/java/com/ufar/studentmanagementsystem/model/Student.java
+++ src/main/java/com/ufar/studentmanagementsystem/model/Student.java
@@ -1,4 +1,15 @@
 package com.ufar.studentmanagementsystem.model;
 
+import java.util.Date;
+
 public class Student {
+    Integer studentID;
+    Integer creatorID;
+    String firstName;
+    String lastName;
+    Date birthDate;
+    String faculty;
+    int year;
+    String degree;
+
 }
--- src/main/java/com/ufar/studentmanagementsystem/model/User.java
+++ src/main/java/com/ufar/studentmanagementsystem/model/User.java
@@ -1,4 +1,7 @@
 package com.ufar.studentmanagementsystem.model;
 
 public class User {
+    Integer userId;
+    String userName;
+    String password;
 }
