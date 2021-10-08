7ff039d2f2f53aab9017f50afa4112aa0abe2d92
Albina Hakobyan <albina.hakobyan7@gmail.com>
2021-10-08 21:20:20 +0400

    user

--- src/main/java/com/ufar/studentmanagementsystem/model/User.java
+++ src/main/java/com/ufar/studentmanagementsystem/model/User.java
@@ -4,4 +4,44 @@ public class User {
     Integer userId;
     String userName;
     String password;
+
+    public User() {
+    }
+
+    public User(Integer userId, String userName, String password) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
