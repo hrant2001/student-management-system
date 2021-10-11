5e27218a62f647ab50c4a0f135181fe7e525d304
Hrant Arakelyan <hrantarakel@gmail.com>
2021-10-11 17:43:36 +0400

    Add 3 layers for student and university entities

---
+++ src/main/java/com/ufar/studentmanagementsystem/controller/StudentController.java
@@ -0,0 +1,57 @@
+package com.ufar.studentmanagementsystem.controller;
+
+import com.ufar.studentmanagementsystem.model.Student;
+import com.ufar.studentmanagementsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findStudents() {
+        List<Student> students = studentService.findStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Student> findStudentById(@PathVariable Integer id) {
+        Student student = studentService.findStudentById(id).orElse(null);
+        if (student == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Student> addStudent(@RequestBody Student newStudent) {
+        Student student = studentService.addStudent(newStudent);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Student> updateStudent(@RequestBody Student updatedStudent) {
+
+        Student student = studentService.updateStudent(updatedStudent).orElse(null);
+        if (student == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteStudentById(@PathVariable Integer id) {
+        studentService.deleteStudentById(id);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/ufar/studentmanagementsystem/controller/UniversityController.java
@@ -0,0 +1,57 @@
+package com.ufar.studentmanagementsystem.controller;
+
+import com.ufar.studentmanagementsystem.model.University;
+import com.ufar.studentmanagementsystem.service.UniversityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/universities")
+public class UniversityController {
+
+    private final UniversityService universityService;
+
+    @Autowired
+    public UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<University>> findUniversities() {
+        List<University> universities = universityService.findUniversities();
+        return new ResponseEntity<>(universities, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<University> findUniversityById(@PathVariable Integer id) {
+        University university = universityService.findUniversityById(id).orElse(null);
+        if (university == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(university, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<University> addUniversity(@RequestBody University newUniversity) {
+        University university = universityService.addUniversity(newUniversity);
+        return new ResponseEntity<>(university, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<University> updateUniversity(@RequestBody University updatedUniversity) {
+
+        University university = universityService.updateUniversity(updatedUniversity).orElse(null);
+        if (university == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(university, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUniversityById(@PathVariable Integer id) {
+        universityService.deleteUniversityById(id);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
